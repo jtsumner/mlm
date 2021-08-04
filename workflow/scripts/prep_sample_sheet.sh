ec37610dcb3c0c1f6643d679eba1122df525de04
Jack Sumner <JackSumner2026@u.northwestern.edu>
2021-08-04 12:47:43 -0500

    changed sample bug

--- workflow/scripts/prep_sample_sheet.sh
+++ workflow/scripts/prep_sample_sheet.sh
@@ -7,5 +7,5 @@ for i in $(ls Batch_0*/*.gz) ; do echo $i | awk -F'_R' '{print $1}' >> tmp.tsv ;
 
 while read p ; do echo $p ; awk -F'/' '{print $2 "\t" $1}' >> samples.tsv ; done < tmp.tsv
 
-sed -i '1s/^/samples\tdataset\n/' samples.tsv
+sed -i '1s/^/sample\tdataset\n/' samples.tsv
 
