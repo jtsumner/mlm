761e4dc3a6c85ce868ede70bf852ec1e00a5380b
Jack Sumner <JackSumner2026@u.northwestern.edu>
2021-08-18 11:29:59 -0500

    changed snakemake run

--- workflow/run_snakemake.sh
+++ workflow/run_snakemake.sh
@@ -2,9 +2,9 @@
 #SBATCH -A p31288
 #SBATCH --job-name="scheduler"
 #SBATCH -t 168:00:00
-#SBATCH -n 10
+#SBATCH -n 2
 #SBATCH -p long
-#SBATCH --mem=2gb
+#SBATCH --mem-per-cpu=3gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jacksumner2026@u.northwestern.edu
 module load anaconda3
