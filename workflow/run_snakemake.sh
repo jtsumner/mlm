553752e616ac1aabae7f180c1b788816904bd2c5
Jack Sumner <JackSumner2026@u.northwestern.edu>
2021-09-07 18:36:11 -0500

    debug cluster

--- workflow/run_snakemake.sh
+++ workflow/run_snakemake.sh
@@ -2,17 +2,17 @@
 #SBATCH -A p31288
 #SBATCH --job-name="scheduler"
 #SBATCH -t 10:00:00
-#SBATCH -n 10
+#SBATCH -n 5
 #SBATCH -p normal
-#SBATCH --mem-per-cpu=3gb
+#SBATCH --mem=3gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jacksumner2026@u.northwestern.edu
 module load anaconda3
-source activate snakemake
-snakemake --version
+conda activate snakemake
+
 # Must be in microbiome-snakemake/workflow/ directory to execute
 cd $SLURM_SUBMIT_DIR
 
 #--max-jobs-per-second 5 --max-status-checks-per-second 5 
 mkdir -p logs_slurm
-snakemake --verbose --use-conda --cluster-config cluster.yaml --max-jobs-per-second 5 --max-status-checks-per-second 5 -j 10 --cluster "sbatch -A {cluster.allocation} -p {cluster.partition} -t {cluster.time} --mem={cluster.mem} -N {cluster.nodes} -n {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type={cluster.email_type} --mail-user={cluster.email} --job-name={cluster.jobname}"
+snakemake --verbose --use-conda --cluster-config cluster.yaml --max-jobs-per-second 1 --max-status-checks-per-second 1 -j 10 --cluster "sbatch -A {cluster.allocation} -p {cluster.partition} -t {cluster.time} --mem={cluster.mem} -N {cluster.nodes} -n {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type={cluster.email_type} --mail-user={cluster.email} --job-name={cluster.jobname}"
