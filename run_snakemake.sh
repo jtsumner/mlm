e3010a8361dcbf6246ecc5057ce7638d36d79b00
jtsumner <jacksumner2026@u.northwestern.edu>
2022-04-15 13:49:44 -0500

    updated sample sheets, .gitignore, and run_snakemake

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -3,9 +3,9 @@
 #SBATCH --job-name="scheduler"
 #SBATCH -t 04:00:00
 #SBATCH -N 1
-#SBATCH -n 2
+#SBATCH -n 5
 #SBATCH -p short
-#SBATCH --mem=5gb
+#SBATCH --mem=20gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jacksumner2026@u.northwestern.edu
 # module load anaconda3
@@ -13,25 +13,12 @@
 
 module purge all
 module load anaconda3
-#source activate mamba
-#source activate updated_snakemake
-#mamba activate updated_snakemake
-source activate snakemake
+source activate mamba
 
-# Must be in microbiome-snakemake/workflow/ directory to execute
 cd $SLURM_SUBMIT_DIR
 
-#--max-jobs-per-second 5 --max-status-checks-per-second 5 
-mkdir -p logs_slurm
-# snakemake --verbose --use-conda --cluster-config cluster.yaml --max-jobs-per-second 1 --max-status-checks-per-second 1 -j 100 --cluster "sbatch -A {cluster.allocation} -p {cluster.partition} -t {cluster.time} --mem={cluster.mem} -N {cluster.nodes} -n {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type={cluster.email_type} --mail-user={cluster.email} --job-name={cluster.jobname}"
-
-snakemake --verbose --use-conda --max-jobs-per-second 1 --max-status-checks-per-second 1 -j 5
 
 echo Starting snakemake on the cluster
-snakemake --verbose \
-    --use-conda \
-    --cluster-config config/cluster.yaml \
-    --max-jobs-per-second 1 \
-    --max-status-checks-per-second 1 \
-    -j 5 \
-    --cluster "sbatch -A {cluster.allocation} -p {cluster.partition} -t {cluster.time} --mem={cluster.mem} -N {cluster.nodes} -n {cluster.cpus} -o {cluster.output} -e {cluster.error} --mail-type={cluster.email_type} --mail-user={cluster.email} --job-name={cluster.jobname}"
+
+snakemake --profile simple
+
