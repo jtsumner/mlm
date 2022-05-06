344e798b0cbac4e7b14f4995b8bad0d097967d1f
jtsumner <jacksumner2026@u.northwestern.edu>
2022-05-06 13:55:33 -0500

    Decorated output file with ASCII art

--- run_snakemake.sh
+++ run_snakemake.sh
@@ -8,17 +8,41 @@
 #SBATCH --mem=20gb
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=jacksumner2026@u.northwestern.edu
+# SBATCH --output="results/slurm_mlm.out"
+# SBATCH --open-mode=append
 # module load anaconda3
 # source activate snakemake
 
+cd $SLURM_SUBMIT_DIR
+
+# Annotating the output file
+cat examples/ascii_art_flowers.txt
+echo "
+NEW SNAKEMAKE EXECUTION :)
+Job Details
+Job ID: ${SLURM_JOB_ID}
+Start Time: ${START_TIME}
+
+Loading conda...
+"
+
+# Load Conda Environment with Snakemake
 module purge all
 module load anaconda3
 source activate mamba
 
-cd $SLURM_SUBMIT_DIR
-
-
-echo Starting snakemake on the cluster
-
+# Execute snakemake
+echo "Starting snakemake on cluster..."
 snakemake --profile simple
 
+
+# Annotating the output file
+END_TIME=$(date)
+echo "
+ENDING SNAKEMAKE EXECUTION
+Job Details
+Job ID: ${SLURM_JOB_ID}
+Start Time: ${END_TIME}
+
+Bye-bye :)
+"
